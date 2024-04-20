9802d68e382c2df335473e21d45ec8246df441e1
Howl Kimura <howlkimuradev@gmail.com>
2024-04-20 15:32:27 +0300

    Connect the styles and create components folder

---
+++ src/components/Clients.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Clients
---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FeedbackCard = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default FeedbackCard
